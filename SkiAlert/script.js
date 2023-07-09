c2d4529bcb5248bdc32d5234f86f28148f4cee6f
AmitAhron88 <aaharon88@gmail.com>
2023-07-10 02:58:31 +0300

    submitting changed to project

--- SkiAlert/script.js
+++ SkiAlert/script.js
@@ -9,12 +9,9 @@ function resortValue(event) {
     event.preventDefault();
     const userInput = document.querySelector('input');
     const inputValue = userInput.value;
-    console.log(inputValue)
     const urlResportValue = inputValue.replace(/ /g, '%20');
     getSkiData(urlResportValue);
-    window.location.href = "result.html";
 }
-
 
 async function getSkiData(resort) {
     const url = `https://ski-resort-forecast.p.rapidapi.com/${resort}/forecast?units=i&el=top`;
@@ -30,8 +27,7 @@ async function getSkiData(resort) {
         const response = await fetch(url, options)
         if(response.ok) {
             const skiObj = await response.json()
-            console.log(skiObj)
-            addForcast(skiObj)
+            addForecast(skiObj)
         } else {
             throw new Error ("issues with fetch")
         }
@@ -40,11 +36,117 @@ async function getSkiData(resort) {
     }
 }
 
-function addForcast(Obj) {
-    const pEl = document.querySelector('p');
-    const pText = document.createTextNode(Obj);
-    pEl.appendChild(pText);
-    document.body.appendChild(pEl);
+function addForecast(Obj) {
+    //location
+    const locationEl = document.getElementById('location');
+    locationEl.textContent = '';
+    const loactionText = document.createTextNode(`Location: ${Obj['basicInfo']['name']}`);
+    locationEl.appendChild(loactionText);
+
+    //3 day forcast
+    const forecastEl = document.getElementById('forecast');
+    forecastEl.textContent = '';
+    const forecastText = document.createTextNode(`3 day forecast: ${Obj['summary3Day']}`);
+    forecastEl.appendChild(forecastText);
+
+    //tomorrows forcast
+    const cardBodyEl = document.getElementsByClassName('cardContainer');
+    for (let element of cardBodyEl) {
+        element.classList.remove('card');
+        element.classList.add('card');
+    };
+
+    const cardHeaderEl = document.getElementById('header');
+    cardHeaderEl.classList.remove('card');
+    cardHeaderEl.classList.add('card-header');
+    cardHeaderEl.textContent = '';
+    const cardHeaderText = document.createTextNode(`Tomorrows Forecast`);
+    cardHeaderEl.appendChild(cardHeaderText);
+
+    const divFooterEl = document.getElementById('footer');
+    divFooterEl.classList.remove('card-footer');
+    divFooterEl.classList.add('card-footer');
+    const cardFooterEl = document.getElementById('cardFooter');
+    const currentDate = new Date();
+    cardFooterEl.textContent = '';
+    const cardFooterText = document.createTextNode(`Last updated: ${currentDate}`);
+    cardFooterEl.appendChild(cardFooterText);
+
+    // AM card
+    const tomorrowsForecast = Obj['forecast5Day']['0']
+    const dayEl = document.getElementById('dayAm');
+    dayEl.textContent = '';
+    const dayText = document.createTextNode(`Day`);
+    dayEl.appendChild(dayText);
+
+    const am = tomorrowsForecast['am'];
+    const summaryEl = document.getElementById('summaryAm');
+    summaryEl.textContent = '';
+    const summaryText = document.createTextNode(`Skies: ${am['summary']}`);
+    summaryEl.appendChild(summaryText);
+
+    const tempEl = document.getElementById('tempAm');
+    tempEl.textContent = '';
+    if (tomorrowsForecast['am']['minTemp'] === am['maxTemp']) {
+        const tempText = document.createTextNode(`Temp: ${am['minTemp']}`);
+        tempEl.appendChild(tempText);
+    } else {
+        const tempText = document.createTextNode(`Temp: ${am['minTemp']} - ${am['maxTemp']}`);
+        tempEl.appendChild(tempText);
+    }
+
+    const windEl = document.getElementById('windAm');
+    windEl.textContent = '';
+    const windText = document.createTextNode(`Wind speed: ${am['windSpeed']}`);
+    windEl.appendChild(windText);
+
+    const snowEl = document.getElementById('snowAm');
+    snowEl.textContent = '';
+    const snowText = document.createTextNode(`Snowfall: ${am['snow']}`);
+    snowEl.appendChild(snowText);
+
+    const rainEl = document.getElementById('rainAm');
+    rainEl.textContent = '';
+    const rainText = document.createTextNode(`Rainfall: ${am['rain']}`);
+    rainEl.appendChild(rainText);
+
+    // PM card
+    const tomorrowsForecastPM = Obj['forecast5Day']['0']
+    const dayPMEl = document.getElementById('dayPm');
+    dayPMEl.textContent = '';
+    const dayPMText = document.createTextNode(`Night`);
+    dayPMEl.appendChild(dayPMText);
+
+    const pm = tomorrowsForecastPM['pm']
+    const summaryPMEl = document.getElementById('summaryPm');
+    summaryPMEl.textContent = '';
+    const summaryPMText = document.createTextNode(`Skies: ${pm['summary']}`);
+    summaryPMEl.appendChild(summaryPMText);
+
+    const tempPMEl = document.getElementById('tempPm');
+    tempPMEl.textContent = '';
+    if (pm['minTemp'] === pm['maxTemp'] ) {
+        const tempPMText = document.createTextNode(`Temp: ${pm['minTemp']}`);
+        tempPMEl.appendChild(tempPMText);
+    } else {
+        const tempPMText = document.createTextNode(`Temp: ${pm['minTemp']} - ${pm['maxTemp']}`);
+        tempPMEl.appendChild(tempPMText);
+    }
+
+    const windPMEl = document.getElementById('windPm');
+    windPMEl.textContent = '';
+    const windPMText = document.createTextNode(`Wind speed: ${pm['windSpeed']}`);
+    windPMEl.appendChild(windPMText);
+
+    const snowPMEl = document.getElementById('snowPm');
+    snowPMEl.textContent = '';
+    const snowPMText = document.createTextNode(`Snowfall: ${pm['snow']}`);
+    snowPMEl.appendChild(snowPMText);
+
+    const rainPMEl = document.getElementById('rainPm');
+    rainPMEl.textContent = '';
+    const rainPMText = document.createTextNode(`Rainfall: ${pm['rain']}`);
+    rainPMEl.appendChild(rainPMText);
 
 }
 
